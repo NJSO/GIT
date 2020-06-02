19ffa2f0bd48109fe55e8b6e95befc4ba3f28550
Nilson Jeronimo <65063242+NJSO@users.noreply.github.com>
2020-06-02 10:02:52 -0300

    adicionando o SQLDRIVER.sh

---
+++ SQLDRIVER.sh
@@ -0,0 +1,47 @@
+# Etapa 1. Instalar o PHP
+echo -e "Etapa 1. Instalar o PHP"
+sudo su
+add-apt-repository ppa:ondrej/php -y
+apt-get update
+apt-get install php7.4 php7.4-dev php7.4-xml -y --allow-
+clear
+
+# instalação do ODBC
+echo -e "Etapa 2. Instalar pré-requisitos"
+echo -e "Instalando o ODBC"
+sudo su
+curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+
+#Download appropriate package for the OS version
+#Choose only ONE of the following, corresponding to your OS version
+
+#Ubuntu 16.04
+curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
+
+#Ubuntu 18.04
+curl https://packages.microsoft.com/config/ubuntu/18.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
+
+#Ubuntu 19.10
+curl https://packages.microsoft.com/config/ubuntu/19.10/prod.list > /etc/apt/sources.list.d/mssql-release.list
+
+exit
+sudo apt-get update
+sudo ACCEPT_EULA=Y apt-get install msodbcsql17
+# optional: for bcp and sqlcmd
+sudo ACCEPT_EULA=Y apt-get install mssql-tools
+echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bash_profile
+echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc
+source ~/.bashrc
+# optional: for unixODBC development headers
+sudo apt-get install unixodbc-dev
+clear
+
+# Etapa 3. Instalar os drivers PHP para Microsoft SQL Server
+echo -e "Etapa 3. Instalar os drivers PHP para Microsoft SQL Server"
+sudo pecl install sqlsrv
+sudo pecl install pdo_sqlsrv
+sudo su
+printf "; priority=20\nextension=sqlsrv.so\n" > /etc/php/7.4/mods-available/sqlsrv.ini
+printf "; priority=30\nextension=pdo_sqlsrv.so\n" > /etc/php/7.4/mods-available/pdo_sqlsrv.ini
+exit
+sudo phpenmod -v 7.4 sqlsrv pdo_sqlsrv
